3f796a9142178ab3503b669398ca479094257a7d
SUDA Mikihrio <mikihiro.suda@gmail.com>
2024-05-18 11:00:15 +0900

    v1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Random Tokyo Station
+
+Show a random station located in Tokyo.
+
+https://sudame.github.io/random-tokyo-station
+
+## Thanks
+
+- [駅データ.jp](https://ekidata.jp/)
---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { ReactElement, useState } from "react";
+import { tokyoStations } from "./tokyoStations";
+import "./App.css";
+
+interface StationView {
+  name: string;
+  fontSizeVw: number;
+}
+
+function getRandomStationView(): StationView {
+  const randomIndex = Math.floor(Math.random() * tokyoStations.length);
+  const stationName = tokyoStations[randomIndex];
+  const fontSizeVw = 60 / stationName.length;
+  return { name: stationName, fontSizeVw };
+}
+
+export function App(): ReactElement {
+  const [stationView, setStationView] =
+    useState<StationView>(getRandomStationView);
+
+  const changeStation = () => {
+    setStationView(getRandomStationView());
+  };
+
+  return (
+    <main className="main">
+      <div
+        className="stationName"
+        style={{ fontSize: `min(5rem, ${stationView.fontSizeVw}vw)` }}
+      >
+        {stationView.name}
+      </div>
+      <div className="googleMap">
+        <a
+          href={`https://google.com/maps/search/${stationView.name}駅`}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Google Map
+        </a>
+      </div>
+      <footer className="changeStation">
+        <button onClick={changeStation}>チェンジ</button>
+      </footer>
+    </main>
+  );
+}
